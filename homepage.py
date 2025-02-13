774817cbc3288b262c96b2547305cefdd2243d13
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-02-13 15:35:39 +0100

    Create homepage.py
    homepage

---
+++ homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def main():
+  pass
+
+
+if __name__ == "__main__":
+    st.title("Olio di Balena Engagement Contests")
+    st.write("Scegli dal menù a tendina a fianco il contest a cui sei interessato!")
+    main()
+  
