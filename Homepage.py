8927be202395ce59a0dcfe619f4188959b227e59
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-02-14 18:55:24 +0000

    Trying to improve the homepage

--- Homepage.py
+++ Homepage.py
@@ -28,28 +28,44 @@ st.markdown(
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
 # ---- Titolo centrato ----
-st.markdown("<h1 style='text-align: center;'>Olio di Balena Engagement Contests</h1>", unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center;'>Olio di Balena Engagement Contests</h1>",
+    unsafe_allow_html=True,
+)
 
 # ---- Introduzione ----
 st.markdown("### Scegli qua sotto il contest a cui sei interessato!")
 
 # ---- Tabella con immagini e link ----
 posts = [
-    {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@balaenoptera/olio-di-balena-scegli-il-migliore-autore-della-settimana-top-author-of-the-week-week-41-jwj"},
-    {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@balaenoptera/untoandbisunto-14-contest-decentralizzato-settimanale-su-olio-di-balena-prize-pool-100-hive-nuovo-tema--vincitori-12-contest"},
-    {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@bencwarmer/eng-ita-odb-engagement-contest"},
+    {
+        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
+        "link": "https://peakd.com/hive-146620/@balaenoptera/olio-di-balena-scegli-il-migliore-autore-della-settimana-top-author-of-the-week-week-41-jwj",
+    },
+    {
+        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
+        "link": "https://peakd.com/hive-146620/@balaenoptera/untoandbisunto-14-contest-decentralizzato-settimanale-su-olio-di-balena-prize-pool-100-hive-nuovo-tema--vincitori-12-contest",
+    },
+    {
+        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
+        "link": "https://peakd.com/hive-146620/@bencwarmer/eng-ita-odb-engagement-contest",
+    },
 ]
 
 # Creazione delle colonne per la visualizzazione delle immagini
 cols = st.columns(len(posts))
 for i, post in enumerate(posts):
     with cols[i]:
-        st.markdown(f'<a href="{post["link"]}" target="_blank"><img src="{post["image"]}" width="100%"></a>', unsafe_allow_html=True)
+        st.markdown(
+            f'<a href="{post["link"]}" target="_blank"><img src="{post["image"]}" width="100%"></a>',
+            unsafe_allow_html=True,
+        )
 
 # ---- Testo sotto le immagini ----
-st.markdown("Sei qui per un po' di dati? Clicca su un'immagine per accedere al post!")
-
+st.markdown(
+    "### Sei qui per un po' di dati? Clicca sul menù a tendina a fianco per accedere a ciò che cerchi!"
+)
