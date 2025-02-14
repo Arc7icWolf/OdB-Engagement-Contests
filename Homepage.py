2c64fb4eb6ceac9911d4bbd2f5486db663390619
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-02-14 22:29:09 +0000

    improving the homepage

--- Homepage.py
+++ Homepage.py
@@ -1,68 +1,120 @@
 import streamlit as st
-
-st.markdown(
-    """
-    <style>
-    /* Sfondo */
-    .stApp {
-        background-image: url("https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg");
-        background-size: cover;
-        background-position: center;
-    }
-
-    /* Stile del testo con bordo nero sfumato */
-    h1, h2, h3, p, .stMarkdown {
-        text-align: center;
-        color: white;
-        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
-    }
-
-    /* Centrare le immagini */
-    .stImage {
-        display: flex;
-        justify-content: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+import requests
+import json
 
 
-# ---- Titolo centrato ----
-st.markdown(
-    "<h1 style='text-align: center;'>Olio di Balena Engagement Contests</h1>",
-    unsafe_allow_html=True,
-)
+# Send request, get response, return decoded JSON response
+def get_response(data, session: requests.Session):
+    urls = [
+        "https://api.deathwing.me",
+        "https://api.hive.blog",
+        "https://hive-api.arcange.eu",
+        "https://api.openhive.network"
+    ]
+    for url in urls:
+        request = requests.Request("POST", url=url, data=data).prepare()
+        response_json = session.send(request, allow_redirects=False)
+        if response_json.status_code == 502:
+            continue
+        response = response_json.json().get("result", [])
+        if len(response) == 0:
+            print(f"{response_json.json()} from this {data}")
+        return response
 
-# ---- Introduzione ----
-st.markdown("### Scegli qua sotto il contest a cui sei interessato!")
 
-# ---- Tabella con immagini e link ----
-posts = [
-    {
-        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
-        "link": "https://peakd.com/hive-146620/@balaenoptera/olio-di-balena-scegli-il-migliore-autore-della-settimana-top-author-of-the-week-week-41-jwj",
-    },
-    {
-        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
-        "link": "https://peakd.com/hive-146620/@balaenoptera/untoandbisunto-14-contest-decentralizzato-settimanale-su-olio-di-balena-prize-pool-100-hive-nuovo-tema--vincitori-12-contest",
-    },
-    {
-        "image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg",
-        "link": "https://peakd.com/hive-146620/@bencwarmer/eng-ita-odb-engagement-contest",
-    },
-]
-
-# Creazione delle colonne per la visualizzazione delle immagini
-cols = st.columns(len(posts))
-for i, post in enumerate(posts):
-    with cols[i]:
-        st.markdown(
-            f'<a href="{post["link"]}" target="_blank"><img src="{post["image"]}" width="100%"></a>',
-            unsafe_allow_html=True,
+def get_posts(session: requests.Session):
+    authors = ["balaenoptera", "balaenoptera", "bencwarmer"]
+    clues = ["Top Author of", "UNTO&BISUNTO:", "OdB Engagement"]
+    contest_posts = []
+    for author in authors:
+        data = (
+            f'{{"jsonrpc":"2.0", "method":"bridge.get_account_posts", '
+            f'"params":{{"sort":"posts", "account": "{author}", '
+            f'"limit": 10}}, "id":1}}'
         )
+        posts = get_response(data, session)
+        for post in posts:
+            author = post["author"]
+            permlink = post["permlink"]
+            title = post["title"]
+            cover = post["json_metadata"].get("image", [])
+            if not cover:
+                cover = post["json_metadata"].get("links", [])
+            for clue in clues:
+                if clue in title:
+                    contest_posts.append({
+                        "image": cover[0].strip(") "), 
+                        "link": "https://peakd.com/@" + author + "/" + permlink,
+                    })
+                    clues.remove(clue)
+                    break
+    
+    return contest_posts
 
-# ---- Testo sotto le immagini ----
-st.markdown(
-    "### Sei qui per un po' di dati? Clicca sul menù a tendina a fianco per accedere a ciò che cerchi!"
-)
+
+def mainpage(session: requests.Session):
+    st.markdown(
+        """
+        <style>
+        /* Sfondo */
+        .stApp {
+            background-image: url("https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg");
+            background-size: cover;
+            background-position: center;
+        }
+
+        /* Stile del testo con bordo nero sfumato */
+        h1, h2, h3, p, .stMarkdown {
+            text-align: center;
+            color: white;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+        }
+
+        /* Centrare le immagini */
+        .stImage {
+            display: flex;
+            justify-content: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    # ---- Titolo centrato ----
+    st.markdown(
+        "<h1 style='text-align: center;'>Olio di Balena Engagement Contests</h1>",
+        unsafe_allow_html=True,
+    )
+
+    # ---- Introduzione ----
+    st.markdown("### Scegli qua sotto il contest a cui sei interessato!")
+
+    # ---- Tabella con immagini e link ----
+    posts = get_posts(session)
+    print(posts)
+
+    # Creazione delle colonne per la visualizzazione delle immagini
+    cols = st.columns(len(posts))
+    for i, post in enumerate(posts):
+        with cols[i]:
+            st.markdown(
+                f'<a href="{post["link"]}" target="_blank"><img src="{post["image"]}" width="100%"></a>',
+                unsafe_allow_html=True,
+            )
+
+    # ---- Testo sotto le immagini ----
+    st.markdown(
+        "### Sei qui per un po' di dati? Clicca sul menù a tendina a fianco per accedere a ciò che cerchi!"
+    )
+
+
+if __name__ == "__main__":
+    try:
+        with requests.Session() as session:
+            mainpage(session)
+    except (json.JSONDecodeError, KeyError) as e:
+        print(f"JSON decode error or missing key: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
