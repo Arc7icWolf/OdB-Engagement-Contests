06d7b08df0375742f08bff4ed21175cfa00b431f
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-02-14 18:47:03 +0000

    Trying to improve the homepage

--- Homepage.py
+++ Homepage.py
@@ -34,7 +34,7 @@ st.markdown("Sei qui per un po' di dati? Clicca su un'immagine per accedere al p
 posts = [
     {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@balaenoptera/olio-di-balena-scegli-il-migliore-autore-della-settimana-top-author-of-the-week-week-41-jwj"},
     {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@balaenoptera/untoandbisunto-14-contest-decentralizzato-settimanale-su-olio-di-balena-prize-pool-100-hive-nuovo-tema--vincitori-12-contest"},
-    {"image": "hhttps://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@bencwarmer/eng-ita-odb-engagement-contest"},
+    {"image": "https://files.peakd.com/file/peakd-hive/arc7icwolf/23wMgpxaWnhqwUSGxBdP7z4VCd6AFzQfGrFYVVcyHLhLjnfZXfhyoJskq4Vha6QAuDbVP.jpeg", "link": "https://peakd.com/hive-146620/@bencwarmer/eng-ita-odb-engagement-contest"},
 ]
 
 # Creazione delle colonne per la visualizzazione delle immagini
